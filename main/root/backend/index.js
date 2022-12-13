b73e80ff1c120222a4a5b8ef23cbdb089f38a55a
ranadip.chatterjee <ranadip.chatterjee@cs.ucalgary.ca>
2022-12-12 20:07:34 -0700

    Add database connection to both local and Atlas

--- main/root/backend/index.js
+++ main/root/backend/index.js
@@ -4,36 +4,53 @@ let cors = require('cors');
 let bodyParser = require('body-parser');
 
 // Express Route
-const UserRoute = require('../backend/routes/user.route')
-const BookRoute = require('../backend/routes/book.route')
+const UserRoute = require('../backend/routes/user.route');
+const BookRoute = require('../backend/routes/book.route');
+//Inspiration: https://www.section.io/engineering-education/nodejs-mongoosejs-mongodb/
+const LocalMongoURI = 'mongodb://127.0.0.1:27017/mydatabase';
+const AtlasURI      = 'mongodb+srv://adminAll:FyJwRDDykhRfxcse@librarycluster.vgreoc9.mongodb.net/testLibrary1';
+//Both databases are the same
 
-// Connecting mongoDB Database
+// Connecting mongoDB Database (https://mongoosejs.com/docs/connections.html)
+/*
 mongoose
-  .connect('mongodb://127.0.0.1:27017/mydatabase')
+  .connect(LocalMongoURI)
   .then((x) => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+    console.log(`Connected to local Mongo database. Database name: "${x.connections[0].name}"`)
   })
-  .catch((err) => {
+  .catch((err) => { 
     console.error('Error connecting to mongo', err.reason)
   })
+*/
+ 
+mongoose.connect(AtlasURI,  { useNewUrlParser: true, useUnifiedTopology: true }).then((x) => {
+    console.log(`Connected to Mongo Atlas cluster! Database name: "${x.connections[0].name}"`)
+  })
+  .catch((err) => {
+    console.error('Error connecting to atlas cluster!', err.reason)
+  })
 
+
+//ROUTING THROUGH EXPRESS FRAMEWORK!
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 app.use(cors());
+//Route for handling requests for path /user
 app.use('/user', UserRoute);
+//Route for handling requests for path /book
 app.use('/book', BookRoute);
 
-
-// PORT
-const port = process.env.PORT || 4000;
+//TODO: ENV file 
+const port =  process.env.PORT || 4000;
 const server = app.listen(port, () => {
-  console.log('Connected to port ' + port)
+  console.log('Backend is now live at port: ' + port)
 })
 
-// 404 Error
+// 404 Error handling (middleware)
 app.use((req, res, next) => {
   next(createError(404));
 });
